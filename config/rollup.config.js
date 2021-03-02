977b927262cfcbf01d48b028f0372706a91bc30e
Kara Rawson <rawsonkara@gmail.com>
2021-03-02 02:12:14 -0500

    setup npm publishing capabilities

--- config/rollup.config.js
+++ config/rollup.config.js
@@ -3,6 +3,11 @@ import commonjs from '@rollup/plugin-commonjs';
 import typescript from '@rollup/plugin-typescript';
 import pkg from '../package.json';
 
+const cfg = {
+    input: "src/index.ts",
+    tsconfig: "config/tsconfig.json"
+}
+
 /**
  * rollup configuration file which builds a commonjs, esm, and universal 
  * javascript module that will be consumed.
@@ -10,7 +15,7 @@ import pkg from '../package.json';
  * @author Kara Rawson <rawsonkara@gmail.com>
  */
 export default [
-    {
+    { // BROWSER JS MODULE (UMD)
         input: 'src/index.ts',
         plugins: [
             resolve({
@@ -18,52 +23,49 @@ export default [
             }),
             typescript({
                 sourceMap: true,
-                tsconfig: "config/tsconfig.json",
-                module: ""
+                tsconfig: "config/tsconfig.json"
             })
         ],
         output: [{ 
             sourcemap: true,
-            file: "lib/as-webgl.umd.js", 
+            file: pkg.browser, 
             name: pkg.name, 
             format: 'umd'
         }]
-    }//,
-    // {
-    //     input: 'src/index.ts',
-    //     plugins: [
-    //         resolve({
-    //             browser: true
-    //         }),
-    //         commonjs(),
-    //         typescript({
-    //             sourceMap: true,
-    //             tsconfig: "config/tsconfig.json"
-    //         })
-    //     ],
-    //     output: [{ 
-    //         file: pkg.main, 
-    //         name: pkg.name, 
-    //         format: 'cjs', 
-    //         sourcemap: true 
-    //     }]
-    // },
-    // {
-    //     input: 'src/index.ts',
-    //     plugins: [
-    //         resolve({
-    //             browser: true
-    //         }),
-    //         typescript({
-    //             sourceMap: true,
-    //             tsconfig: "config/tsconfig.json"
-    //         })
-    //     ],
-    //     output: [{ 
-    //         file: pkg.module, 
-    //         name: pkg.name, 
-    //         format: 'es', 
-    //         sourcemap: true 
-    //     }]
-    // }
+    }, { // ESM JS BUNDLE 
+        input: 'src/index.ts',
+        plugins: [
+            resolve({
+                browser: true
+            }),
+            typescript({
+                sourceMap: true,
+                tsconfig: "config/tsconfig.json"
+            })
+        ],
+        output: [{ 
+            sourcemap: true,
+            file: pkg.module, 
+            name: pkg.name, 
+            format: 'es'
+        }]
+    }, { // COMMON JS BUNDLE
+        input: 'src/index.ts',
+        plugins: [
+            resolve({
+                browser: true
+            }),
+            commonjs(),
+            typescript({
+                sourceMap: true,
+                tsconfig: "config/tsconfig.json"
+            })
+        ],
+        output: [{ 
+            sourcemap: true,
+            file: pkg.main, 
+            name: pkg.name, 
+            format: 'cjs'
+        }]
+    }
 ];
