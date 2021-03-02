a2030daadfaad4472ad0498a2f458ec223dbd391
Kara Rawson <rawsonkara@gmail.com>
2021-03-01 23:05:44 -0500

    add ts compiler with rollup for `lib` npm

---
+++ config/rollup.config.js
@@ -0,0 +1,69 @@
+import resolve from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
+import typescript from '@rollup/plugin-typescript';
+import pkg from '../package.json';
+
+/**
+ * rollup configuration file which builds a commonjs, esm, and universal 
+ * javascript module that will be consumed.
+ * 
+ * @author Kara Rawson <rawsonkara@gmail.com>
+ */
+export default [
+    {
+        input: 'src/index.ts',
+        plugins: [
+            resolve({
+                browser: true
+            }),
+            typescript({
+                sourceMap: true,
+                tsconfig: "config/tsconfig.json",
+                module: "ESNext"
+            })
+        ],
+        output: [{ 
+            sourcemap: true,
+            file: pkg.browser, 
+            name: pkg.name, 
+            format: 'umd'
+        }]
+    }//,
+    // {
+    //     input: 'src/index.ts',
+    //     plugins: [
+    //         resolve({
+    //             browser: true
+    //         }),
+    //         commonjs(),
+    //         typescript({
+    //             sourceMap: true,
+    //             tsconfig: "config/tsconfig.json"
+    //         })
+    //     ],
+    //     output: [{ 
+    //         file: pkg.main, 
+    //         name: pkg.name, 
+    //         format: 'cjs', 
+    //         sourcemap: true 
+    //     }]
+    // },
+    // {
+    //     input: 'src/index.ts',
+    //     plugins: [
+    //         resolve({
+    //             browser: true
+    //         }),
+    //         typescript({
+    //             sourceMap: true,
+    //             tsconfig: "config/tsconfig.json"
+    //         })
+    //     ],
+    //     output: [{ 
+    //         file: pkg.module, 
+    //         name: pkg.name, 
+    //         format: 'es', 
+    //         sourcemap: true 
+    //     }]
+    // }
+];
---
+++ src/foobar.ts
@@ -0,0 +1,7 @@
+export function foobar(bar: string): string  {
+    return bar
+}
+
+function print(msg: string): void {
+    console.log(msg)
+}
